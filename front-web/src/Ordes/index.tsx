26c8206955cb6de6d021a57de4b46b609aeb43c0
Fernando Coutinho <fa2c1@hotmail.com>
2021-01-08 23:41:46 -0300

    Add Order

--- front-web/src/Ordes/index.tsx
+++ front-web/src/Ordes/index.tsx
@@ -1,10 +1,21 @@
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '../api';
+import ProductsList from './ProductsList';
 import StepsHeader from './StepHeader';
 import './styles.css';
+import { Product } from './types';
 
 function Orders(){
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        fetchProducts().then(Response => setProducts(Response.data)).catch(error => console.log(error));
+    }, []);
+
     return (
         <div className="ORDERS-CONTAINER">
             <StepsHeader />
+            <ProductsList products={products}/>
         </div>
     )
 }
