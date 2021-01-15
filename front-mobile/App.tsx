33da5cefd8bcd758ca28a05ab48428e6592cf0f5
Fernando Coutinho <fa2c1@hotmail.com>
2021-01-15 20:52:25 -0300

    Fix Error Sintaxe

---
+++ front-mobile/App.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useFonts, OpenSans_400Regular, OpenSans_700Bold} from "@expo-google-fonts/open-sans";
+import AppLoading from "expo-app-loading";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Header from "./src/Header/Index";
+import Home from "./src/Home";
+
+export default function App() {
+  let [fontsLoaded] = useFonts({
+    OpenSans_400Regular,
+    OpenSans_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Header />
+      <Home />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
